0cf236b961c4e15fba3f3848d9f1a44d57aa1010
Santiago Bacaro <sbacarob@outlook.com>
2018-10-28 19:37:09 +0100

    initial commit. Empty project

---
+++ lib/vantagex.ex
@@ -0,0 +1,18 @@
+defmodule Vantagex do
+  @moduledoc """
+  Documentation for Vantagex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vantagex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vantagex_test.exs
@@ -0,0 +1,8 @@
+defmodule VantagexTest do
+  use ExUnit.Case
+  doctest Vantagex
+
+  test "greets the world" do
+    assert Vantagex.hello() == :world
+  end
+end
