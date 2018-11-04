a3f7a214bebc06cc5605493d24c03b229d40099c
Santiago Bacaro <sbacarob@outlook.com>
2018-11-04 12:56:29 +0100

    update elixir version to 1.7
    correct word in README

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Vantagex.Mixfile do
     [
       app: :vantagex,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
