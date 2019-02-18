346dd84eaf805de76b56db23633dc0b7dbbe0f5c
Santiago Bacaro <sbacarob@outlook.com>
2019-02-18 23:55:02 +0100

    add daily function

--- lib/vantagex/forex.ex
+++ lib/vantagex/forex.ex
@@ -35,7 +35,7 @@ defmodule Vantagex.Forex do
           "7. Time Zone" => "UTC"
         }
       }
-  
+
       iex> Vantagex.Forex.currency_exchange_rate("USD", "COP", datatype: :json)
       "{\\n    \"Realtime Currency Exchange Rate\": {\\n        \"1. From_Currency Code\": \"USD\",\\n        \"2. From_Currency Name\": \"United States Dollar\",\\n        \"3. To_Currency Code\": \"COP\",\\n        \"4. To_Currency Name\": \"Colombian Peso\",\\n        \"5. Exchange Rate\": \"3130.00000000\",\\n        \"6. Last Refreshed\": \"2019-02-16 22:34:00\",\\n        \"7. Time Zone\": \"UTC\"\\n    }\\n}"
   """
@@ -102,6 +102,63 @@ defmodule Vantagex.Forex do
     } |> clean_params()
 
     resolve_request(:intraday, params)
+  end
+
+  @doc """
+  Uses Alpha Vantage's `FX_DAILY` function.
+  Returns the daily time series of the FX currency pair specified.
+
+  Args:
+
+  * `from_symbol` - three letter string representing the currency. e.g. `"EUR"`
+  * `to_symbol` - three letter string representing the currency. e.g. `"USD"`
+  * `opts` - A list of extra options to pass to the function.
+
+  Allowed options:
+
+  * `outputsize` - `:compact | :full` when set to compact returns the latest 100
+  datapoints; when set to full returns the full length intraday time series. Defaults to compact
+  * `datatype` - `:map | :json | :csv` specifies the return format. Defaults to :map
+
+  ## Examples
+
+      iex> Vantagex.Forex.daily("USD", "COP")
+      %{
+        "Meta Data" => %{
+          "1. Information" => "Forex Daily Prices (open, high, low, close)",
+          "2. From Symbol" => "USD",
+          "3. To Symbol" => "COP",
+          "4. Output Size" => "Compact",
+          "5. Last Refreshed" => "2019-02-19 06:40:00",
+          "6. Time Zone" => "GMT+8"
+        },
+        "Time Series FX (Daily)" => %{
+          "2018-11-12" => %{
+            "1. open" => "3178.5000",
+            "2. high" => "3178.5000",
+            "3. low" => "3170.3000",
+            "4. close" => "3174.3000"
+          },
+          "2018-12-06" => %{
+            "1. open" => "3159.0000",
+            "2. high" => "3191.8000",
+            "3. low" => "3154.3000",
+            "4. close" => "3184.2000"
+          },
+          ...
+        }
+      }
+  """
+  @spec daily(String.t(), String.t(), Keyword.t()) :: String.t() | Map.t()
+  def daily(from_symbol, to_symbol, opts \\ []) do
+    params = %{
+      from_symbol: from_symbol,
+      to_symbol: to_symbol,
+      outputsize: Keyword.get(opts, :outputsize),
+      datatype: Keyword.get(opts, :datatype)
+    } |> clean_params()
+
+    resolve_request(:daily, params)
   end
 
   defp clean_params(params) do
